c800753127fe5ea7ecde5c1844a7364a4ce6d4a5
Aziz Hararwala <ahararwala@pillartechnology.com>
2017-04-30 16:12:14 -0400

    Reformated

--- src/CalculateJacobian.cpp
+++ src/CalculateJacobian.cpp
@@ -24,9 +24,9 @@ int computeJacobain() {
     return 0;
 }
 
-MatrixXd CalculateJacobian(const VectorXd& x_state) {
+MatrixXd CalculateJacobian(const VectorXd &x_state) {
 
-    MatrixXd Hj(3,4);
+    MatrixXd Hj(3, 4);
     //recover state parameters
     float px = x_state(0);
     float py = x_state(1);
@@ -34,20 +34,20 @@ MatrixXd CalculateJacobian(const VectorXd& x_state) {
     float vy = x_state(3);
 
     //pre-compute a set of terms to avoid repeated calculation
-    float c1 = px*px+py*py;
+    float c1 = px * px + py * py;
     float c2 = sqrt(c1);
-    float c3 = (c1*c2);
+    float c3 = (c1 * c2);
 
     //check division by zero
-    if(fabs(c1) < 0.0001){
+    if (fabs(c1) < 0.0001) {
         cout << "CalculateJacobian () - Error - Division by Zero" << endl;
         return Hj;
     }
 
     //compute the Jacobian matrix
-    Hj << (px/c2), (py/c2), 0, 0,
-            -(py/c1), (px/c1), 0, 0,
-            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+    Hj << (px / c2), (py / c2), 0, 0,
+            -(py / c1), (px / c1), 0, 0,
+            py * (vx * py - vy * px) / c3, px * (px * vy - py * vx) / c3, px / c2, py / c2;
 
     return Hj;
 }
